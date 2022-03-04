381c32a6bc0f4dbfeb63f9c5cdea5e3f1c2e0c80
Abhay Garg <gargabhay2000@gmail.com>
2022-03-05 01:32:38 +0530

    Add errorMiddleware

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,30 @@
 const express = require("express");
-const app = express();
 const colors = require("colors");
 const dotenv = require("dotenv").config();
 const connectDb = require("./config/db");
 const port = process.env.PORT || 5000;
+const { errorHandler } = require("./middleware/errorMiddleware");
 
+// Connect to MonogoDB.
 connectDb();
+
+const app = express();
+
+// Add middlewares to parse json requests and url encoded bodies
+// of requests.
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// TODO: @Gautam Add user routes here.
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// Middleware to handle custom errors in `production` and
+// `development` server.
+app.use(errorHandler);
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Cflockout app listening on port ${port}`);
 });
