d3e37539907f624f4056d23d088d270656644379
Gautam Patodi <56384002+WolverienG6@users.noreply.github.com>
2022-03-06 15:25:30 +0530

    Add create contest endpoint (#6)
    Add create contest route

--- backend/controllers/contestController.js
+++ backend/controllers/contestController.js
@@ -1,4 +1,5 @@
 const asyncHandler = require("express-async-handler");
+const { validationResult } = require("express-validator");
 const Contest = require("../models/contestModel");
 
 /** Fetches all contests related to a user. */
@@ -8,7 +9,34 @@ const getContests = asyncHandler(async (req, res) => {
 });
 
 /** Creates a new contest. */
-const createContest = asyncHandler(async (req, res) => {});
+const createContest = asyncHandler(async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const { duration, problems } = req.body;
+
+  const admin = req.user._id;
+  const users = [admin];
+  const contestants = [
+    {
+      username: req.user.username,
+      userId: req.user._id,
+      points: 0,
+      rank: 1,
+    },
+  ];
+
+  const contest = await Contest.create({
+    users,
+    admin,
+    duration,
+    problems,
+    contestants,
+  });
+
+  res.status(201).json(contest);
+});
 
 /** Updates a goal with the given id. */
 const joinContest = asyncHandler(async (req, res) => {});
--- backend/routes/contestRoutes.js
+++ backend/routes/contestRoutes.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const { body } = require("express-validator");
+const {
+  MIN_NUMBER_OF_PROBLEMS,
+  MAX_NUMBER_OF_PROBLEMS,
+  MIN_DURATION,
+  MAX_DURATION,
+  MIN_PROBLEM_POINTS,
+  MAX_PROBLEM_POINTS,
+} = require("../config/constants");
 
 const {
   getContests,
@@ -11,7 +20,33 @@ const {
 const { protect } = require("../middleware/authMiddleware");
 
 router.route("/all").get(protect, getContests);
-router.route("/create").post(protect, createContest);
+router.route("/create").post(
+  [
+    // Validating number of problems
+    body(
+      "problems",
+      `Number of problems should be in between ${MIN_NUMBER_OF_PROBLEMS} and ${MAX_NUMBER_OF_PROBLEMS} `
+    ).isArray({
+      min: MIN_NUMBER_OF_PROBLEMS,
+      max: MAX_NUMBER_OF_PROBLEMS,
+    }),
+    // Validating contest duration
+    body(
+      "duration",
+      `Contest Duration should be in between ${MIN_DURATION} minutes and ${MAX_DURATION} minutes`
+    ).isNumeric({ min: MIN_DURATION, max: MAX_DURATION }),
+    // Validating points of every problem
+    body(
+      "problems.*.points",
+      `Points of each problem should lie between ${MIN_PROBLEM_POINTS} and ${MAX_PROBLEM_POINTS}`
+    ).isFloat({
+      min: MIN_PROBLEM_POINTS,
+      max: MAX_PROBLEM_POINTS,
+    }),
+  ],
+  protect,
+  createContest
+);
 router.route("/join/:id").put(protect, joinContest);
 router.route("/invalidate/:id").post(protect, invalidateContest);
 
