0dbc9737b6700f52a2560a37ba44a9ec9e0622a1
Abhay Garg <gargabhay2000@gmail.com>
2022-03-23 10:18:18 +0530

    changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -46,16 +46,13 @@ app.use("/api/contests", require("./routes/contestRoutes"));
 // Serve frontend.
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
-
   app.get("*", (req, res) =>
     res.sendFile(
       path.resolve(__dirname, "../", "frontend", "build", "index.html")
     )
   );
 } else {
-  app.get("/", (req, res) =>
-    res.send("This is dev server. Please set NODE_ENV to production.")
-  );
+  app.get("/", (req, res) => res.send("Please go in production."));
 }
 
 // Middleware to handle custom errors in `production` and
