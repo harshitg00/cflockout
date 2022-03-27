ad335bbd2b464b387efc383bc720741301af1e32
Abhay Garg <gargabhay2000@gmail.com>
2022-03-27 14:47:50 +0530

    Add README

---
+++ README.md
@@ -0,0 +1,10 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app), using the [Redux](https://redux.js.org/) and [Redux Toolkit](https://redux-toolkit.js.org/) template.
+
+## Installation
+
+1. Fork and clone the repository locally.
+2. Go to the root and copy the environmet variables from .env.example. Make a new .env file in the root and paste the environment variables here.
+3. In the root, run command `$ npm run setup`
+   to install frontend and backend dependencies.
+4. To run the development server locally, run -
+   `$ npm run dev`
--- frontend/src/pages/CreateContest.jsx
+++ frontend/src/pages/CreateContest.jsx
@@ -2,9 +2,7 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useState, useEffect } from "react";
 import { Grid, TextField } from "@mui/material";
@@ -17,6 +15,7 @@ import {
 import { reset, createContest } from "../features/contest/contestSlice";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import CodeIcon from "@mui/icons-material/Code";
 
 const CreateContest = () => {
   const theme = createTheme();
@@ -78,7 +77,6 @@ const CreateContest = () => {
   };
 
   const handleRemoveProblem = (removedProblemIndex) => {
-    // console.log("Remove", removedProblemIndex);
     let newProblems = contestProblems.filter(
       (problem, index) => index !== removedProblemIndex
     );
@@ -97,98 +95,106 @@ const CreateContest = () => {
 
   return (
     <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Create a new Lockout Contest
-          </Typography>
-          <TextField
-            name="duration"
-            id="duration"
-            fullWidth
-            label="Duration of the contest"
-            autoFocus
-            variant="outlined"
-            type="number"
-            value={duration}
-            inputProps={{ min: MIN_DURATION, max: MAX_DURATION }}
-            onChange={onChangeDuration}
-          />
+      <CssBaseline />
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ mt: "4%", bgcolor: "secondary.main" }}>
+          <CodeIcon />
+        </Avatar>
+        <Typography component="h1" variant="h4" sx={{ m: "4%" }}>
+          Create a new Lockout Contest
+        </Typography>
+        <TextField
+          name="duration"
+          id="duration"
+          fullWidth
+          margin="normal"
+          label="Duration of the contest"
+          autoFocus
+          variant="outlined"
+          type="number"
+          value={duration}
+          inputProps={{ min: MIN_DURATION, max: MAX_DURATION }}
+          onChange={onChangeDuration}
+        />
 
-          {contestProblems.map((problem, index) => {
-            return (
-              <Grid key={index} container spacing={4} sx={{ marginY: 2 }}>
-                <Grid item xs={2}>
+        {contestProblems.map((problem, index) => {
+          return (
+            <Grid
+              key={index}
+              container
+              spacing={4}
+              sx={{ marginY: 2, alignItems: "center" }}
+            >
+              <Grid item xs={2}>
+                <Box
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
                   <DeleteRoundedIcon
+                    cursor="pointer"
+                    className="svg_icons"
                     id="remove"
                     onClick={() => {
-                      // console.log("In onClick", problem);
                       handleRemoveProblem(index);
                     }}
                   />
-                </Grid>
-                <Grid item xs={5}>
-                  <TextField
-                    name="points"
-                    id="points"
-                    label="Points"
-                    autoFocus
-                    fullWidth
-                    variant="outlined"
-                    type="number"
-                    value={problem.points}
-                    inputProps={{ min: 1, max: 100 }}
-                    onChange={onChangeProblem(index)}
-                  />
-                </Grid>
-                <Grid item xs={5}>
-                  <TextField
-                    name="rating"
-                    id="rating"
-                    fullWidth
-                    label="Codeforces Rating"
-                    autoFocus
-                    variant="outlined"
-                    type="number"
-                    value={problem.rating}
-                    inputProps={{ step: 100, min: 800, max: 2500 }}
-                    onChange={onChangeProblem(index)}
-                  />
-                </Grid>
+                </Box>
               </Grid>
-            );
-          })}
+              <Grid item xs={5}>
+                <TextField
+                  name="points"
+                  id="points"
+                  label="Points"
+                  autoFocus
+                  fullWidth
+                  variant="outlined"
+                  type="number"
+                  value={problem.points}
+                  inputProps={{ min: 1, max: 100 }}
+                  onChange={onChangeProblem(index)}
+                />
+              </Grid>
+              <Grid item xs={5}>
+                <TextField
+                  name="rating"
+                  id="rating"
+                  fullWidth
+                  label="Codeforces Rating"
+                  autoFocus
+                  variant="outlined"
+                  type="number"
+                  value={problem.rating}
+                  inputProps={{ step: 100, min: 800, max: 2500 }}
+                  onChange={onChangeProblem(index)}
+                />
+              </Grid>
+            </Grid>
+          );
+        })}
 
-          <Button variant="contained" onClick={handleAddProblem}>
-            + Add problem
-          </Button>
-          <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit}
-            sx={{ mt: 3 }}
+        <Button variant="contained" onClick={handleAddProblem} sx={{ mt: 4 }}>
+          + Add problem
+        </Button>
+        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 2, mb: 2 }}
           >
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Create Contest
-            </Button>
-          </Box>
+            Create Contest
+          </Button>
         </Box>
-      </Container>
+      </Box>
     </ThemeProvider>
   );
 };
